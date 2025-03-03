0caada84d02e1b7c7872157eedad6d49d2a4d693
Joseph Duchesne <joseph@upsiderobotics.com>
2025-03-03 16:08:22 +0000

    comitted linktrack

--- nlink_parser_ros2/src/utils/init_serial.cpp
+++ nlink_parser_ros2/src/utils/init_serial.cpp
@@ -20,53 +20,54 @@ void enumerate_ports() {
 
 bool initSerial(serial::Serial *serial, std::string port_name, uint32_t baudrate)
 {
-try
-  {
-    YAML::Node port_config = YAML::LoadFile(param_file_path)["port_config"];
-    YAML::const_iterator it = port_config.begin();
-    std::map<std::string, std::string> port_config_dict;
-    for (; it != port_config.end(); ++it)
-    {
-        auto key = it->first.as<std::string>();
-        port_config_dict[key] = it->second.as<std::string>();
-    }
-    // auto get_value = [&](const std::string& key)-> void{
-    //   auto _value = port_config_dict.find(key);
-    //   if(_value==port_config_dict.end()){
-    //     std::cout<< "Failed to get param: "<< key<<", please check the config file and retry."<<std::endl;
-    //     exit(EXIT_FAILURE);
-    //   } else {
-    //     return _value->second();
-    //   }
-    // };
+// try
+//   {
+//     YAML::Node port_config = YAML::LoadFile(param_file_path)["port_config"];
+//     YAML::const_iterator it = port_config.begin();
+//     std::map<std::string, std::string> port_config_dict;
+//     for (; it != port_config.end(); ++it)
+//     {
+//         auto key = it->first.as<std::string>();
+//         port_config_dict[key] = it->second.as<std::string>();
+//     }
+//     // auto get_value = [&](const std::string& key)-> void{
+//     //   auto _value = port_config_dict.find(key);
+//     //   if(_value==port_config_dict.end()){
+//     //     std::cout<< "Failed to get param: "<< key<<", please check the config file and retry."<<std::endl;
+//     //     exit(EXIT_FAILURE);
+//     //   } else {
+//     //     return _value->second();
+//     //   }
+//     // };
 
      
-    if(port_config_dict.find("port_name")==port_config_dict.end() || port_config_dict.find("baudrate")==port_config_dict.end()){
-      std::cout<< "Failed to get a param please check the config file and retry."<<std::endl;
-      exit(EXIT_FAILURE);
-    }
-    std::string port_name, baudrate;
-    auto search = port_config_dict.find("port_name");
-    if(search==port_config_dict.end()){
-      port_name = "/dev/ttyUSB1";
-    } else {
-      port_name = search->second;
-    }
+//     if(port_config_dict.find("port_name")==port_config_dict.end() || port_config_dict.find("baudrate")==port_config_dict.end()){
+//       std::cout<< "Failed to get a param please check the config file and retry."<<std::endl;
+//       exit(EXIT_FAILURE);
+//     }
+//     std::string port_name, baudrate;
+//     auto search = port_config_dict.find("port_name");
+//     if(search==port_config_dict.end()){
+//       port_name = "/dev/ttyUSB1";
+//     } else {
+//       port_name = search->second;
+//     }
     
-    search = port_config_dict.find("baudrate");
-    if(search==port_config_dict.end()){
-      baudrate = "921600";
-    } else {
-      baudrate = search->second;    
-    }
-    uint32_t baud_rate_int = static_cast<uint32_t>(std::stoi(baudrate));
-    initSerial(serial, port_name, baud_rate_int);
-  }
-  catch (const std::exception &e)
-  {
-    std::cout<< "Exception raised : "<<e.what()<<std::endl;
-    exit(EXIT_FAILURE);
-  }
+//     search = port_config_dict.find("baudrate");
+//     if(search==port_config_dict.end()){
+//       baudrate = "921600";
+//     } else {
+//       baudrate = search->second;    
+//     }
+//     uint32_t baud_rate_int = static_cast<uint32_t>(std::stoi(baudrate));
+//     initSerial(serial, port_name, baud_rate_int);
+//   }
+//   catch (const std::exception &e)
+//   {
+//     std::cout<< "Exception raised : "<<e.what()<<std::endl;
+//     exit(EXIT_FAILURE);
+//   }
+  return false; // Not currently working
 }
 
 bool initSerial(serial::Serial *serial, const std::string& port_name, const uint32_t baudrate)
@@ -76,7 +77,7 @@ bool initSerial(serial::Serial *serial, const std::string& port_name, const uint
   serial->setBaudrate(baudrate);
 
   std::cout<< "try to open serial port with port: " << port_name << " baud rate: "<< baudrate << std::endl;
-  auto timeout = serial::Timeout::simpleTimeout(10);
+  auto timeout = serial::Timeout::simpleTimeout(2);
   // without setTimeout,serial can not write any data
   // https://stackoverflow.com/questions/52048670/can-read-but-cannot-write-serial-ports-on-ubuntu-16-04/52051660?noredirect=1#comment91056825_52051660
   serial->setTimeout(timeout);
--- nlink_parser_ros2/src/utils/init_serial.h
+++ nlink_parser_ros2/src/utils/init_serial.h
@@ -1,6 +1,7 @@
 #ifndef INITSERIAL_H
 #define INITSERIAL_H
 #include <serial/serial.h>
+#include <yaml-cpp/yaml.h>
 
 bool initSerial(serial::Serial *serial, const std::string& port_name, const uint32_t baudrate);
 bool initSerial(serial::Serial *serial, char *param_file_path);
